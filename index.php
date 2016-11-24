92c7b47d094f947adfc25e5f7ec18642b2c96392
tutorial <tutorial@gmail.com>
2016-11-24 20:27:10 +0000

    Refactoring assets location.

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 <meta charset="utf-8">
 <title>Responsive jQuery Slider with fadeIn and fadeOut effect</title>
 <link rel="shortcut icon" href="logos/favicon.ico" type="image/x-icon" />
-<script src="/jquery_slider_fade_effect/jquery-1.12.0.js" type="text/javascript"></script>
+<script src="/jquery-1.12.0.js" type="text/javascript"></script>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
-<link rel="stylesheet" type="text/css" href="/jquery_slider_fade_effect/css/main.css" />
-<link rel="stylesheet" type="text/css" href="/jquery_slider_fade_effect/font-awesome/css/font-awesome.min.css" />
-<script type="text/javascript" src="/jquery_slider_fade_effect/main.js"></script>
+<link rel="stylesheet" type="text/css" href="/css/main.css" />
+<link rel="stylesheet" type="text/css" href="/font-awesome/css/font-awesome.min.css" />
+<script type="text/javascript" src="/main.js"></script>
 </head>
 <body>
   <div id="container">
@@ -17,12 +17,12 @@
     
     <div class="slider_container">
       <ul>
-        <li class="current_slide"><img src="/jquery_slider_fade_effect/images/image1.jpeg" alt="slide image"></li>
-        <li><img src="/jquery_slider_fade_effect/images/image2.jpeg" alt="slide image"></li>
-        <li><img src="/jquery_slider_fade_effect/images/image3.jpeg" alt="slide image"></li>
-        <li><img src="/jquery_slider_fade_effect/images/image4.jpeg" alt="slide image"></li>
-        <li><img src="/jquery_slider_fade_effect/images/image5.jpeg" alt="slide image"></li>
-        <li><img src="/jquery_slider_fade_effect/images/image6.jpeg" alt="slide image"></li>
+        <li class="current_slide"><img src="/images/image1.jpeg" alt="slide image"></li>
+        <li><img src="/images/image2.jpeg" alt="slide image"></li>
+        <li><img src="/images/image3.jpeg" alt="slide image"></li>
+        <li><img src="/images/image4.jpeg" alt="slide image"></li>
+        <li><img src="/images/image5.jpeg" alt="slide image"></li>
+        <li><img src="/images/image6.jpeg" alt="slide image"></li>
       </ul>
       
       <span class="prev_slide"><i class="fa fa-angle-left" aria-hidden="true"></i></span>
